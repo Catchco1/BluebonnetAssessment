49779492be549f0d0ab5793c320d94c2c2fd918f
Connor Gephart <connor.gephart@jhuapl.edu>
2024-06-13 12:50:21 +0000

    add requirements and finish assignment

--- wrangling.py
+++ wrangling.py
@@ -17,12 +17,11 @@ for file in generalFiles:
 
 generalDf = pd.concat(dfList, axis=0, ignore_index=True)
 
-print(generalDf)
-
 generalDf.to_csv('output/2021_special_generals.csv')
 
 # Set up Primary Election CSV
 
+# To run this elsewhere, please make sure this path makes sense
 primaryPath = r'data/2021SpecialPrimaries/'
 
 primaryFiles = glob.glob(os.path.join(primaryPath, "*.xlsx"))
@@ -35,6 +34,15 @@ for file in primaryFiles:
 
 primaryDf = pd.concat(dfList, axis=0, ignore_index=True)
 
-print(primaryDf)
-
 primaryDf.to_csv('output/2021_special_primaries.csv')
+
+# Find Duplicates
+elections = {'2021_special_primaries.csv': primaryDf, '2021_special_generals.csv': generalDf}
+
+for dataset, election in elections.items():
+    duplicates = election[election.duplicated('VOTER ID')]
+    if duplicates.empty:
+        print('No Duplicates Found in {}!'.format(dataset))
+    else:
+        print('Duplicates Found in {}!'.format(dataset))
+        print(duplicates)
